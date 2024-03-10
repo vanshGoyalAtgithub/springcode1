7b5df5074972884cfc783a910ea6d13415f57b71
vanshgoyalatgithub <vansh.goel.0403@gmail.com>
2024-03-10 17:18:51 +0530

    solved more errors and get some more

--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,8 +1,12 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.*;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@ComponentScan
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Role findByName(String name);
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.example.demo.repository;
 import com.example.demo.entity.User; // Add missing import
 // Add missing import
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
+@ComponentScan(basePackages = "com.example.demo")
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
